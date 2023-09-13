207376a4249ed191b2b5449348378416938d3b3a
student <ashurov1308@mail.ru>
2023-09-13 12:39:48 +0300

    add files for subscriptions list and one subscription

--- transport_subscriptions_back_app/views.py
+++ transport_subscriptions_back_app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
 
-def hello(request):
-    return render(request, 'index.html')
+def GetSubscriptions(request):
+    return render(request, 'subscriptions.html', {'data' : {
+        'current_date': date.today(),
+        'subscriptions': [
+            {'title': 'Книга с картинками', 'id': 1},
+            {'title': 'Бутылка с водой', 'id': 2},
+            {'title': 'Коврик для мышки', 'id': 3},
+        ]
+    }})
+
+def GetSubscription(request, id):
+    return render(request, 'sunbcription.html', {'data' : {
+        'current_date': date.today(),
+        'id': id
+    }})
