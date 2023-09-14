db85ade38d2be3473b664cacbce8a85e10b88627
student <ashurov1308@mail.ru>
2023-09-14 11:08:57 +0300

    base ui for subscriptions page

--- transport_subscriptions_back/settings.py
+++ transport_subscriptions_back/settings.py
@@ -115,8 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/transport_subscriptions_back_app/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "transport_subscriptions_back_app/static/"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- transport_subscriptions_back_app/views.py
+++ transport_subscriptions_back_app/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date
 
+# subscriptionsData =
+
 def GetSubscriptions(request):
     return render(request, 'subscriptions.html', {'data' : {
         'current_date': date.today(),
         'subscriptions': [
-            {'title': 'Книга с картинками', 'id': 1},
-            {'title': 'Бутылка с водой', 'id': 2},
-            {'title': 'Коврик для мышки', 'id': 3},
+            {'title': 'Метро', 'id': 1},
+            {'title': 'МЦД', 'id': 2},
+            {'title': 'МЦК', 'id': 3},
+            {'title': 'Автобусы/троллейбусы/трамваи', 'id': 4},
+            {'title': 'Велосипеды', 'id': 5},
+            {'title': 'Самокаты', 'id': 6},
         ]
     }})
 
