289b72695c4ffadff8424604a12121d8e2f1d417
Ashurov Georgy <ashurov1308@mail.ru>
2023-09-21 09:23:17 +0000

    edit styles for subscription card component

--- transport_subscriptions_back/settings.py
+++ transport_subscriptions_back/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = '/transport_subscriptions_back_app/static/'
-STATIC_URL = '/static/'
+STATIC_URL = '/transport_subscriptions_back_app/static/'
+# STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "transport_subscriptions_back_app/static/"
