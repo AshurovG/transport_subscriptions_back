da94a3386a0e8f41da63ad79157892669b7b8441
Ashurov <ashurov1308@mail.ru>
2023-09-26 11:57:03 +0300

    finish rendering detailed subsctiption data from data base. Edit filter

--- transport_subscriptions_back_app/views.py
+++ transport_subscriptions_back_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from transport_subscriptions_back_app.models import *
 
-
 def getSubscriptionsData():
     return [
             {'title': 'Метро / МЦК', 'id': 1, 'src': 'images/subway.jpg', 'info': 'Стоимости тарифов зависят от количества поездок и варьируются от 250р. до 1300р.', 'rates':[{'title': '5 поездок', 'price': '250р.'}, {'title': '15 поездок', 'price': '700р.'}, {'title': '30 поездок', 'price': '1300р.'}],},
@@ -12,31 +11,42 @@ def getSubscriptionsData():
             {'title': 'Самокаты', 'id': 5, 'src': 'images/scooter.jpeg', 'info': 'Стоимости тарифов зависят от колчиства дней, на которое вы приобритаете абонемент. Можно приобрести абонемент на определенное количесто дней от 30 до 365, и цена будет составлять от 400р. до 3500р. Обратите внимание, что абонемент распространяется только на бесплатный старт проезда на самокате, последующие минуты проезда необходимо оплачивать по 7р./минута', 'rates':[{'title': 'Бесплатный старт 30 дней', 'price': '400р.'}, {'title': 'Бесплатный старт 90 дней', 'price': '1000р.'}, {'title': 'Бесплатный старт 365 дней', 'price': '3500р.'}],},
         ]
 
+
+# def GetVacancies(request):
+#     keyword = request.GET.get('keyword')
+#     a = Vacancies.objects.filter(status='enabled')
+#     if keyword:
+#          keyword = keyword[0].upper()+keyword[1:]
+#          a = Vacancies.objects.filter(status='enabled').filter(title=keyword)
+#     return render(request, 'vacancies.html', {'data': {
+#         'current_date': date.today(),
+#         'vacancies': a},
+#         "search_query": keyword if keyword else ""})
+
+# def GetVacancy(request, id):
+#     return render(request, 'vacancy.html', {'data' : {
+#         'current_date': date.today(),
+#         'vacancy': Vacancies.objects.get(id = id)
+#     }})
+
 def GetSubscriptions(request):
     query = request.GET.get("sub")
-    subs = getSubscriptionsData()
-    res = []
-    
-    for sub in subs:
-        if (query is not None):
-            if query.lower() in sub["title"].lower():
-                res.append(sub)
-        else:
-            res = subs
-            query = ''
+    subs = Subscriptions.objects.filter(status='enabled')
 
+    if query:
+         subs = Subscriptions.objects.filter(status='enabled').filter(title__icontains=query)
+    # if query:
+    #      print(11111)
+    #      subs = Subscriptions.objects.filter(status='enabled').filter(title=query)
+    else:
+        query = ''
+    
     return render(request, 'subscriptions.html', {'data' : {
-        'subscriptions': res,
+        'subscriptions': subs,
         'inputValue': query
     }})
 
 def GetSubscription(request, id):
-    subscriptionsData = getSubscriptionsData()
-    subscription = next((sub for sub in subscriptionsData if sub['id'] == id), None)
-    if subscription:
-        print(subscription['rates'])
-    else:
-        print("Not found")
     return render(request, 'subscription.html', {'data' : {
-        'subscription': subscription
+        'subscription': Subscriptions.objects.get(id = id)
     }})
