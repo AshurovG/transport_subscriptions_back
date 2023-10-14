4c619bce9aa2d871ea6010467d27b452b64ad5f7
Ashurov <ashurov1308@mail.ru>
2023-10-14 13:56:22 +0300

    clear project

--- transport_subscriptions_back/urls.py
+++
@@ -1,8 +0,0 @@
-from transport_subscriptions_back_app import views
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', views.hello),
-]
--- transport_subscriptions_back_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TransportSubscriptionsBackAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transport_subscriptions_back_app'
