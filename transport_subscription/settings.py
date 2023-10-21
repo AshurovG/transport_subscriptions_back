d8da3e6ab94d1938aa0fbc3f9c994249ad8d3cbd
Ashurov <ashurov1308@mail.ru>
2023-10-21 13:03:04 +0300

    add permission to receive data from api

--- transport_subscription/settings.py
+++ transport_subscription/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'transport_subscription_app',
 ]
@@ -49,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'transport_subscription.urls'
 
